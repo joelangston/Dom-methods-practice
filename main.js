788a951d8892d76f2cb99bd31138e9fea624f234
Joe Langston <joelangston81@gmail.com>
2020-06-16 13:29:33 -0500

    main stuff done

---
+++ main.js
@@ -0,0 +1,28 @@
+const bodyElement = document.querySelector("body")
+
+const buttonElement = document.createElement("button")
+bodyElement.append(buttonElement)
+buttonElement.append("button")
+
+const mainElement = document.createElement("main")
+bodyElement.append(mainElement)
+const imageElement = document.createElement("img")
+imageElement.src = "https://www.nba.com/.element/img/1.0/teamsites/logos/teamlogos_500x500/bos.png"
+imageElement.alt = "Boston Celtics"
+mainElement.append(imageElement)
+
+const linkElement = document.createElement("a")
+linkElement.href = "https://www.nba.com/celtics/video/originals/filmstudy-walker-uconn-061220"
+linkElement.target = "_blank"
+linkElement.append(" Boston Celtics")
+mainElement.append(linkElement)
+
+buttonElement.addEventListener("click", function() {
+    mainElement.remove()
+})
+
+
+
+
+
+
